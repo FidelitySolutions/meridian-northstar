73c1284a9de81c1ba713b9541f405fbe6bb07eaf
Jordan Tate <jordan@meridiansoftware.example>
2025-06-23 12:00:00 +0000

    feat: build lab results list and detail view ui scaffold with awaiting-results empty state using stub data

---
+++ client/src/components/LabResults/LabResultDetail.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LabResultDetail: React.FC = () => {
+  return <div>LabResultDetail</div>;
+};
+
+export default LabResultDetail;
---
+++ client/src/pages/LabResultsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LabResultsPage: React.FC = () => {
+  return <div>LabResultsPage</div>;
+};
+
+export default LabResultsPage;
